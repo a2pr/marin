6dd59208e50d73501c72b8df02929830bf9fd34f
Andres Payema <a2payema@gmail.com>
2022-07-17 18:34:56 -0400

    nowt working with promises

--- Controller/OrderController.js
+++ Controller/OrderController.js
@@ -1,9 +1,9 @@
 import OrderModel from "../Database/models/orderModel.js";
 import * as orderRepository from "../Repository/orderRepository.js";
 
-export const getOrders = (req, res, next) => {
+export const getOrders = async (req, res, next) => {
 
-    let orders = orderRepository.getAllOrders();
+    let orders = await orderRepository.getAllOrders();
     console.log(orders)
     res.status(200).json(orders)
     next();
@@ -11,19 +11,36 @@ export const getOrders = (req, res, next) => {
 
 export const getById = (req, res, next) => {
     console.log(`get request by ${req.params.id}`)
+    let result = false
 
-    res.status(200).json(
-        orderRepository.getOrderId(req.body.id)
+    try {
+        result = orderRepository.getOrderId(req.body.id)
+    } catch (e) {
+        console.error(`Something wrong happen ${e}`);
+    }
+
+    res.status(result ? 200 : 400).json(
+        {
+            "result": result
+        }
     )
-
     next();
 }
 
 export const getByTitle = (req, res, next) => {
     console.log(`get request by title`)
+    let result = false
 
-    res.status(200).json(
-        orderRepository.getOrdersTitle(req.body.title)
+    try {
+        result = orderRepository.getOrdersTitle(req.body.title)
+    } catch (e) {
+        console.error(`Something wrong happen ${e}`);
+    }
+
+    res.status(result ? 200 : 400).json(
+        {
+            "result": result
+        }
     )
     next();
 }
@@ -32,8 +49,20 @@ export const postNew = (req, res, next) => {
     console.log(`post new`)
 
     let requestOrder = OrderModel.fromRequest(req.body)
-    res.status(200).json(
-        orderRepository.insertOrder(requestOrder)
+    let result = false;
+
+    try {
+        result = orderRepository.insertOrder(requestOrder)
+    } catch (e) {
+        console.error(`Something wrong happen ${e}`)
+    }
+
+
+    res.status(result ? 200 : 400).json(
+        {
+            "result": result
+        }
     )
+
     next();
 }
